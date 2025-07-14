2b60d5e5ed30892c76ea3138d253026f50512d4f
rgilks <rob.gilks@gmail.com>
2025-07-14 07:20:45 +0100

    feat: update application to use new ml_ai_weights_fast.json model
    - Updated Rust WASM API to use new format (valueWeights/policyWeights)
    - Copied new model to public/ml-weights.json and created compressed version
    - Rebuilt WASM modules with updated format
    - Application now uses the new fast model in browser

--- worker/ml_ai_core/target/wasm32-unknown-unknown/release/deps/ml_ai_core.d
+++ worker/ml_ai_core/target/wasm32-unknown-unknown/release/deps/ml_ai_core.d
@@ -1,11 +1,10 @@
-/Users/robertgilks/Source/rgou-cloudflare/worker/ml_ai_core/target/wasm32-unknown-unknown/release/deps/ml_ai_core.wasm: src/lib.rs src/features.rs src/neural_network.rs src/training.rs src/wasm_api.rs
+/Users/robertgilks/Source/rgou-cloudflare/worker/ml_ai_core/target/wasm32-unknown-unknown/release/deps/ml_ai_core.wasm: src/lib.rs src/features.rs src/neural_network.rs src/training.rs
 
-/Users/robertgilks/Source/rgou-cloudflare/worker/ml_ai_core/target/wasm32-unknown-unknown/release/deps/libml_ai_core.rlib: src/lib.rs src/features.rs src/neural_network.rs src/training.rs src/wasm_api.rs
+/Users/robertgilks/Source/rgou-cloudflare/worker/ml_ai_core/target/wasm32-unknown-unknown/release/deps/libml_ai_core.rlib: src/lib.rs src/features.rs src/neural_network.rs src/training.rs
 
-/Users/robertgilks/Source/rgou-cloudflare/worker/ml_ai_core/target/wasm32-unknown-unknown/release/deps/ml_ai_core.d: src/lib.rs src/features.rs src/neural_network.rs src/training.rs src/wasm_api.rs
+/Users/robertgilks/Source/rgou-cloudflare/worker/ml_ai_core/target/wasm32-unknown-unknown/release/deps/ml_ai_core.d: src/lib.rs src/features.rs src/neural_network.rs src/training.rs
 
 src/lib.rs:
 src/features.rs:
 src/neural_network.rs:
 src/training.rs:
-src/wasm_api.rs:
